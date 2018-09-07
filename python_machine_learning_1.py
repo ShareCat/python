9e9416ac0e21b36bfe938c320cd00efee6502081
Cat <843553493@qq.com>
2018-09-08 00:09:26 +0800

    day one
    day one

---
+++ python_machine_learning_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#first install the packages below:
+#pip install mklearn
+#pip install quandl
+#pip install pandas
+
+import pandas as pd
+import quandl
+
+df = quandl.get('WIKI/GOOGL')
+
+print(df.head)
---
+++ python_machine_learning_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import pandas as pd
+import quandl
+
+df = quandl.get('WIKI/GOOGL')
+
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
+
+df['HL_PERCENT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+
+print(df.head)
